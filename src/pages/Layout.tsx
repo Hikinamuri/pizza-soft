2f459d31fabaa728dd32405afca2b14a1fa3c06e
Hikinamuri <Hikinamuri@mail.ru>
2024-08-23 00:02:13 +0300

    delete: extra files

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,9 +1,10 @@
 import React from "react"
 import { Outlet } from "react-router-dom"
+import cl from './Layout.module.scss'
 
 export const Layout = () => {
     return (
-        <div>
+        <div className={cl.layout}>
             <Outlet />
         </div>
     )
