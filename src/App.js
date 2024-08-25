d345cee9bbd21b3491233daddbe23ac8808da081
Hikinamuri <Hikinamuri@mail.ru>
2024-08-25 10:31:05 +0300

    added redirect to /home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router-dom";
 import { Layout } from './pages/Layout.tsx';
 import { Home } from './pages/home/homePage.tsx';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Layout />}>
-        <Route path='/home' element={<Home />} />
+      <Route path='/' element={<Navigate to='/home' />} />
+      <Route path='/home' element={<Layout />}>
+        <Route index element={<Home />} />
       </Route>
     </Routes>
   );
